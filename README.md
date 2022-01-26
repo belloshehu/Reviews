58b80d5e121068c02932be51cba05ea50ac7242b
Shehu Bello <belloshehu1@gmail.com>
2022-01-26 01:14:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reviews
+React front-end application for displaying a review at a time. 
+
+The "suprise me" button when clicked, displays a random review.
---
+++ src/Review.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const {name, image, text, job} = people[index]
+
+  const checkNumber = (number)=>{
+    if(number > people.length - 1){
+      return 0;
+    }if(number < 0) {
+      return people.length - 1;
+    }
+    return number
+  }
+  const prevPerson = ()=>{
+    let newIndex = index - 1
+    setIndex((prev)=> checkNumber(newIndex))
+  }
+
+  const nextPerson = ()=>{
+    let newIndex = index + 1
+    setIndex((prev)=> checkNumber(newIndex))
+  }
+
+  const getRandomPerson = ()=>{
+    let randomIndex = Math.floor(Math.random() * people.length)
+    if(randomIndex === index){
+      randomIndex = index - 1
+    }
+    setIndex(checkNumber(randomIndex))
+  }
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+          <button className='prev-btn' onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className='next-btn' onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+      </div>
+      <button className='random-btn' onClick={getRandomPerson}>Suprise me</button>
+    </article>
+  )
+};
+
+export default Review;
